dbb68dbae913d02a517e91221ad695a9ec136424
Amal TH <amal@Amals-MacBook-Air.local>
2026-05-14 19:31:39 +0200

    calender

--- src/index.js
+++ src/index.js
@@ -30,6 +30,10 @@ require("./handlers/automod")(client);
 client.once("ready", async () => {
   console.log(`Logged in as ${client.user.tag}`);
 
+  await client.application.commands.create({
+  name: "calendar",
+  description: "Shows the AQW boost calendar for May 2026"
+});
   try {
     await client.application.commands.create({
       name: "ping",
